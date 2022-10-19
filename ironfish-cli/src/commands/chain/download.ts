052a65b1eb4cd2b3f8a1fa470285e401bb21047e
Joseph Parks <jowparks@gmail.com>
2022-10-19 09:10:09 -0700

    feat: boilerplated for chunked download, WIP

--- ironfish-cli/src/commands/chain/download.ts
+++ ironfish-cli/src/commands/chain/download.ts
@@ -10,7 +10,7 @@ import {
   VERSION_DATABASE_CHAIN,
 } from '@ironfish/sdk'
 import { CliUx, Flags } from '@oclif/core'
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import crypto from 'crypto'
 import fs from 'fs'
 import fsAsync from 'fs/promises'
@@ -295,3 +295,125 @@ export default class Download extends IronfishCommand {
     speed.stop()
   }
 }
+
+// type ChunkConfig = {
+//   range: { startByte: number; endByte: number }
+//   retry: number
+//   onError: () => void
+// }
+
+// async function downloadChunk(url: string, config: ChunkConfig) {
+//   const { range, retry = 1, onError } = config
+//   const { startByte, endByte } = range
+//   const cfg = {
+//     responseType: 'arraybuffer',
+//     headers: { range: `bytes=${startByte}-${endByte}` },
+//   }
+//   const checksumCfg = {
+//     headers: { range: `bytes=${startByte}-${endByte}`, check_sum: 1 },
+//   }
+
+//   const checkSum: { data: IncomingMessage } = await axios.get(url, checksumCfg)
+
+//   let filePath
+//   for (let i = 0; i < retry; ++i) {
+//     try {
+//       const result = await _downloadToFile(url, cfg)
+//       filePath = result.filePath
+//       if (checkSum === result.checksum) {
+//         return filePath
+//       } else {
+//         try {
+//           fs.unlinkSync(filePath)
+//         } catch (e) {} // remove invalid file
+//         onError &&
+//           onError(
+//             { message: `Checksum mismatch. Expect: ${checkSum}, Received: ${result.checksum}` },
+//             i,
+//           )
+//       }
+//     } catch (e) {
+//       try {
+//         fs.unlinkSync(filePath)
+//       } catch (e) {} // remove invalid file if exist
+//       onError && onError(e, i)
+//     }
+//   }
+
+//   throw 'Download failed'
+// }
+
+// /**
+//  * @param {String | Object} src download url
+//  * @param {String} src.url: CDN url
+//  * @param {String} src.checksumUrl: checkSum Url
+//  * @param config
+//  * @param {Boolean} [config.skipCheck]: Skip checksum checking step
+//  * @param {Number} [config.retry=1]: Number the number of time try to re-download if failed
+//  * @param {Function} [config.onError]: Error handle function to handle error e at download number i
+//  * @returns {Promise<void>}
+//  */
+// async function download(src: string, config) {
+//   const chunkSizeInBytes = config.chunkSizeInBytes || 1024 * 1024 // 1 MB
+//   if (chunkSizeInBytes <= 0) {
+//     throw `Invalid chunk size: ${chunkSizeInBytes}`
+//   }
+
+//   console.log('Downloading chunks...')
+//   const filePaths = []
+//   let start = 0
+//   while (true) {
+//     const filePath = await downloadChunk(src, {
+//       ...config,
+//       range: { start, length: chunkSizeInBytes },
+//     })
+//     filePaths.push(filePath)
+//     const fileStat = fs.statSync(filePath)
+//     if (fileStat.size !== chunkSizeInBytes) {
+//       break
+//     }
+//     start += chunkSizeInBytes
+//   }
+
+//   console.log('Joining chunks...')
+//   const outputFile = config.outputPath || tmp.tmpNameSync()
+//   for (const filePath of filePaths) {
+//     await _appendFile(filePath, outputFile)
+//     fs.unlinkSync(filePath)
+//   }
+
+//   return outputFile
+// }
+
+// function _appendFile(fsrc, fdest) {
+//   return new Promise((resolve, reject) => {
+//     const f1 = fs.createReadStream(fsrc)
+//     const f2 = fs.createWriteStream(fdest, { flags: 'a' })
+//     f2.on('error', reject)
+//     f2.on('finish', resolve)
+//     f1.pipe(f2)
+//   })
+// }
+
+// async function _downloadToFile(url: string, config: AxiosRequestConfig) {
+//   config.responseType = 'stream'
+
+//   const filePath = tmp.tmpNameSync()
+//   const writer = fs.createWriteStream(filePath)
+//   const dataStream = (await axios.get(url, config)).data
+//   const state = StreamMD5.init()
+//   dataStream
+//     .pipe(
+//       though2(function (chunk, enc, callback) {
+//         StreamMD5.update(state, chunk)
+//         this.push(chunk)
+//         callback()
+//       }),
+//     )
+//     .pipe(writer)
+
+//   return new Promise((resolve, reject) => {
+//     writer.on('finish', () => resolve({ filePath, checksum: StreamMD5.finalize(state) }))
+//     writer.on('error', reject)
+//   })
+// }
