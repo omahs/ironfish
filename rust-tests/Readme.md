c8ef2d878a4c7c71ee031c119b4788fbd2b6aaf3
danield9tqh <danield9tqh@gmail.com>
2022-10-20 17:27:22 -0700

    add rust perf tests for combine_hash

---
+++ rust-tests/Readme.md
@@ -0,0 +1,14 @@
+#Running Test
+1. Download [cargo-flamegraph](https://github.com/flamegraph-rs/flamegraph). 
+```bash
+cargo install flamegraph
+```
+
+2. Run the flamegraph in this folder. You will need to run with root permissions and enter password
+```bash
+cargo build
+cargo run flamegraph --root
+```
+
+3. SVG image is outputted in the same folder, open with Chrome
+
---
+++ rust-tests/src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+extern crate hex;
+
+// use rand::Rng;
+use hex::decode;
+
+use ironfish_rust::MerkleNoteHash;
+
+fn main() {
+    for _ in 1..1000000 {
+
+        let left = decode("a2801e0582bef67bfb1250556a5f163e50d853178e23874457b79b356fc6265f").unwrap();
+        let right = decode("81d4d87dd0cbab8ba8371c80b5cc1a4b03f9f7609fe9f038d534d0fc996c5068").unwrap();
+        // let right = rand::thread_rng().gen::<[u8; 32]>();
+        let depth = 10;
+
+        let left_hash = MerkleNoteHash::read((&left[..]).as_ref()).unwrap();
+        let right_hash = MerkleNoteHash::read((&right[..]).as_ref()).unwrap();
+
+        let converted_depth: usize = depth
+            .try_into()
+            .unwrap();
+
+        let mut vec = Vec::with_capacity(32);
+
+        MerkleNoteHash::new(MerkleNoteHash::combine_hash(
+            converted_depth,
+            &left_hash.0,
+            &right_hash.0,
+        ))
+        .write(&mut vec)
+        .unwrap();
+    }
+
+    println!("Finished");
+}
